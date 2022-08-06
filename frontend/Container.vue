6d6a8ceb5dc5112f2eaf0c3c70a41633778e1b88
Joe Sweeney <joe.sweeney224@gmail.com>
2022-08-06 12:05:05 -0600

    color fun

--- frontend/Container.vue
+++ frontend/Container.vue
@@ -2,7 +2,7 @@
 <div class="d-flex">
   <div 
     class="rounded-start border border-end-0 px-3 py-2 w-100"
-    :class="[container.State == 'running' ? 'bg-primary text-white border-0' : '']"
+    :class="[container.State == 'running' ? 'bg-teal border-0' : '']"
   >
     {{ container.Names[0].substring(1) }}
   </div>
@@ -13,7 +13,7 @@
     </button>
   </div>
   <div v-else class="h-100">
-    <button v-if="container.State == 'running'" class="btn btn-secondary rounded-0 h-100" @click="stopContainer(container.Id)"><fai icon="fa-solid fa-stop"/></button>
+    <button v-if="container.State == 'running'" class="btn btn-purple rounded-0 h-100" @click="stopContainer(container.Id)"><fai icon="fa-solid fa-stop"/></button>
     <button v-else class="btn btn-success rounded-0 h-100" @click="startContainer(container.Id)"><fai icon="fa-solid fa-play"/></button>
   </div>
   <button class="btn btn-danger rounded-0 rounded-end"><fai icon="fa-solid fa-trash"/></button>
